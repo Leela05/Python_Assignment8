8981540e0fd250809c537f1b4cab0221a79941f9
Leela05 <leelarenu05@gmail.com>
2022-03-18 23:48:42 +0530

    first commit

--- prgm1.py
+++ prgm1.py
@@ -2,3 +2,12 @@
 # a method show to display the coordinates of the point
 # a method move to change these coordinates
 
+# 1.To show points coordinate in a plot in Python, we can take the following steps −
+
+# Set the figure size and adjust the padding between and around the subplots.
+# Create lists of x and y data points.
+# Plot x and y data points with red color and starred marker
+# Set some axis properties.
+# Iterate x and y to show the coordinates on the plot.
+
+
